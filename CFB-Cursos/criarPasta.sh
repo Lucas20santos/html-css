25ef553919427ad46de8b8c23bc5094d8ffe2414
LUCAS DE SOUZA SANTOS <lucass.santoss210@gmail.com>
2025-07-02 13:28:07 +0000

    final da aula da tag base.

--- CFB-Cursos/criarPasta.sh
+++ CFB-Cursos/criarPasta.sh
@@ -4,6 +4,10 @@ read -p "Informe o valor inicial: " inicio
 read -p "Informe o valor final: " final
 
 for i in $(seq $inicio $final); do
-    mkdir "Aula $i"
-    touch "Aula $i/Aula$i.html"
+    if [ -e "/workspaces/html-css/CFB-Cursos/Aula $i" ]; then
+        echo "-> Aula $i, pasta existente."
+    else
+        mkdir /workspaces/html-css/CFB-Cursos/'Aula '$i
+        touch /workspaces/html-css/CFB-Cursos/'Aula '$i/Aula$i.html
+    fi
 done
